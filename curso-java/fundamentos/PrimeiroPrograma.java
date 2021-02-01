5d654efd110c86ecd4d0d7e5368f95779ff6b407
Daiane Barbosa Farias <daiaanebarbosaf@gmail.com>
2021-02-01 16:27:18 -0300

    Create PrimeiroPrograma.java

---
+++ curso-java/fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,18 @@
+package fundamentos;
+
+public class PrimeiroPrograma {
+
+	/**
+	 * comentário JAVADOC
+	 * PARA COLOCAR INFORMAÇÕES DO SEU CODIGO
+	 * @author krotonadm
+	 * */
+	public static void main(String[] args) {
+		System.out.println("Olá Mundo!");
+	}
+}
+
+	//comentario de uma linha
+	/*
+	 * comentario de mais de duas linhas
+	 */
