010014cfc05a2da5599db1a1f125f6424327d1be
Daiane Barbosa Farias <daiaanebarbosaf@gmail.com>
2021-02-17 14:14:18 -0300

    Rename Inferencia.java to 03 - Inferencia.java

--- curso-java/fundamentos/03 - Inferencia.java
+++ curso-java/fundamentos/03 - Inferencia.java
@@ -4,13 +4,13 @@ public class Inferencia {
 	
 	public static void main(String[] args) {
 		
-		double a = 4.5; //declaraÁ„o e inicializaÁ„o da vari·vel
+		double a = 4.5; //declara√ß√£o e inicializa√ß√£o da vari√°vel
 		System.out.println(a);
 		
 		/**
-		 * Inferencia È quando vocÍ n„o
-		 * coloca o tipo de vari·vel e
-		 * o prÛprio programa reconhece 
+		 * Inferencia √© quando voc√™ n√£o
+		 * coloca o tipo de vari√°vel e
+		 * o pr√≥prio programa reconhece 
 		 * e exibe o valor de acordo com
 		 * as caracteristicas do valor.
 		 * Exemplo a baixo:
@@ -26,14 +26,14 @@ public class Inferencia {
 		System.out.println(c);
 		
 		/*Se foi definido um determinado valor para
-		 * uma vari·vel e depois vocÍ querer mudar 
-		 * n„o È possivel, pois essa j· foi previamente
+		 * uma vari√°vel e depois voc√™ querer mudar 
+		 * n√£o √© possivel, pois essa j√° foi previamente
 		 * inferida como String
 		 * Ex:
 		 * c = 4.5;
 			System.out.println(c);*/
 		
-		double d; //vari·vel for declarada
+		double d; //vari√°vel for declarada
 		d = 123.65; //variavel foi inicializada
 		System.out.println(d);// usada, foi impresso o valor
 	
