e789cf157a1f5f6a56c66cdbbb1086d6178c9965
Daiane Barbosa Farias <daiaanebarbosaf@gmail.com>
2021-03-03 21:18:16 -0300

    Create 03 - CelsiusFahrenheit.java

---
+++ curso-java/fundamentos/Exercícios do Módulo/03 - CelsiusFahrenheit.java
@@ -0,0 +1,15 @@
+package ex;
+
+public class CelsiusFahrenheit {
+
+	public static void main(String[] args) {
+		
+		double celsius = 5;
+		
+		double temperatura = (celsius * 9.0/5.0) + 32;
+		
+		System.out.println(" Temperatura em Fahrenheit é: "  + temperatura +  " ° F " );
+
+	}
+
+}
