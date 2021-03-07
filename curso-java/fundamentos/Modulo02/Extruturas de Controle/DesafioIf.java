9192cd753fcd4caa788224f07d6f425e9dee22f3
Daiane Barbosa Farias <daiaanebarbosaf@gmail.com>
2021-03-07 18:07:34 -0300

    Create DesafioIf.java

---
+++ curso-java/fundamentos/Modulo02/Extruturas de Controle/DesafioIf.java
@@ -0,0 +1,22 @@
+package estruturasDeControle;
+
+public class DesafioIf {
+
+	public static void main(String[] args) {
+		
+		double nota = 1.3;
+		
+		/*o ponto e virgula após o 9.0, faz com que a
+		 * comparação não seja feita e então ele
+		 * mostra o resultado sem comparar se a
+		 * nota é maior ou igual a 9.*/
+		if(nota >= 9.0); {
+			System.out.println("Quadro de Honra!");
+			System.out.println("Você é fera!!!");
+		}/*else {
+			System.out.println("Não foi dessa vez!");
+		}*/
+
+	}
+
+}
