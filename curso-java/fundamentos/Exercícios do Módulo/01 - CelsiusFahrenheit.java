792de11cff6f447f51042264b6dcb6bfe25aa20e
Daiane Barbosa Farias <daiaanebarbosaf@gmail.com>
2021-03-07 11:07:47 -0300

    Update 01 - CelsiusFahrenheit.java

--- curso-java/fundamentos/Exercícios do Módulo/01 - CelsiusFahrenheit.java
+++ curso-java/fundamentos/Exercícios do Módulo/01 - CelsiusFahrenheit.java
@@ -1,14 +1,20 @@
 package ex;
 
+import java.util.Scanner;
+
 public class CelsiusFahrenheit {
 
 	public static void main(String[] args) {
+		Scanner leitor = new Scanner(System.in);
 		
-		double celsius = 5;
+		System.out.println("Digite a temperatura em Celsius: ");
+		double celsius = leitor.nextDouble();
 		
-		double temperatura = (celsius * 9.0/5.0) + 32;
+		double temperatura = (celsius - 32) / 1.8;
 		
 		System.out.println(" Temperatura em Fahrenheit é: "  + temperatura +  " ° F " );
+
+		leitor.close();
 
 	}
 
