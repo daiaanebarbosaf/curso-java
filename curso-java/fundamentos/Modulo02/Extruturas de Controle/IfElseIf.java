325eb492995ead27de17f2617d451ce36de42b92
Daiane Barbosa Farias <daiaanebarbosaf@gmail.com>
2021-03-07 18:07:52 -0300

    Create IfElseIf.java

---
+++ curso-java/fundamentos/Modulo02/Extruturas de Controle/IfElseIf.java
@@ -0,0 +1,33 @@
+package estruturasDeControle;
+
+import java.util.Scanner;
+
+public class IfElseIf {
+
+	public static void main(String[] args) {
+		
+		Scanner leitor = new Scanner (System.in);
+		
+		System.out.println("Digite uma nota: ");
+		double nota = leitor.nextDouble();
+		
+		if(nota > 10 || nota < 0) {
+			System.out.println("Nota Inválida");
+		}else if(nota >= 8.1){
+			System.out.println("Conceito A");
+		}else if(nota >= 6.1){
+			System.out.println("Conceito B");
+		}else if(nota >= 4.1){
+			System.out.println("Conceito C");
+		}else if(nota >= 2.1){
+			System.out.println("Conceito D");
+		}else {
+			System.out.println("Conceito E");
+		}
+		
+		leitor.close();
+
+	}
+	
+
+}
