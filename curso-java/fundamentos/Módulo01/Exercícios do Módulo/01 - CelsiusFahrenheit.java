7bff66abab4ad45d8cf0991d1bbfecd6bfe8d252
Daiane Barbosa Farias <daiaanebarbosaf@gmail.com>
2021-03-07 18:00:46 -0300

    Create 01 - CelsiusFahrenheit.java

---
+++ curso-java/fundamentos/Módulo01/Exercícios do Módulo/01 - CelsiusFahrenheit.java
@@ -0,0 +1,18 @@
+package fundamentos;
+
+public class temperatura {
+	
+	public static void main(String[] args) {
+		
+		double f = 8;
+		
+		final int a = 32;
+		final double multi = 5.0/9.0;
+		
+		double temperatura = (f - a) * multi;
+		
+		System.out.println("Temperatura em Graus Celsius: " + temperatura + "°C");
+		
+	}
+
+}
