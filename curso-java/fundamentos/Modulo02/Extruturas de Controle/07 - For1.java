539fe83725ff23302a6c95c801303b12ff0f1184
Daiane Barbosa Farias <daiaanebarbosaf@gmail.com>
2021-03-17 17:38:09 -0300

    Create 07 - For1.java

---
+++ curso-java/fundamentos/Modulo02/Extruturas de Controle/07 - For1.java
@@ -0,0 +1,23 @@
+package EstruturasControle;
+
+public class For1 {
+
+	public static void main(String[] args) {
+		
+		/*int contador = 0;
+		
+		while (contador <= 10) {
+			
+			System.out.println("Bom dia!");
+			contador++;
+		}*/
+		
+		
+		for(int contador = 0; contador<= 10; contador ++) {
+			
+			System.out.println("contador = " + contador);
+		}
+		
+	}
+
+}
