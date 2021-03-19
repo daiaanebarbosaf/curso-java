df7cbb850208ec6a48c783568278606ef9b091db
Daiane Barbosa Farias <daiaanebarbosaf@gmail.com>
2021-03-19 18:08:01 -0300

    Create 09 - DoWhile.java

---
+++ curso-java/fundamentos/Modulo02/Extruturas de Controle/09 - DoWhile.java
@@ -0,0 +1,34 @@
+package EstruturasControle;
+
+import java.util.Scanner;
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+		
+		//if(...) sentença; ou {}
+		//while(...) sentença; ou {}
+		//for(...;...;...) senteça; ou {}
+		/*do sentença; ou {} while (...); executa
+		 * pelo menos uma vez, diferente do while
+		 * que se for o caso nem precisa executar */
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		String texto = "";
+		
+		do {
+			
+			System.out.println("Você precisa falar "
+					+ "as palavras mágicas: ");
+			System.out.println("Quer sair? ");
+			
+			texto = entrada.nextLine();
+			
+		} while(!texto.equalsIgnoreCase("por favor"));
+		
+		System.out.println("Obrigado e Fim!");
+
+	}
+
+}
