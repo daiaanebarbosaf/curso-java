2ccffaac1805645380c3362e6a44f167bc3ddbee
Daiane Barbosa Farias <daiaanebarbosaf@gmail.com>
2021-03-20 18:28:29 -0300

    Update 11 - For2.java

--- curso-java/fundamentos/Modulo02/Extruturas de Controle/11 - For2.java
+++ curso-java/fundamentos/Modulo02/Extruturas de Controle/11 - For2.java
@@ -4,7 +4,9 @@ public class For2 {
 
 	public static void main(String[] args) {
 		
-		for (int contador = 10; contador >= 0; contador -=2) {
+		int contador;
+		
+		for (contador = 10; contador >= 0; contador -=2) {
 			
 			System.out.println("Contador = " + contador);
 			
