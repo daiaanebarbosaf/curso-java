078124a9834a59054d030f73479722bd32ab0f91
Daiane Barbosa Farias <daiaanebarbosaf@gmail.com>
2021-03-20 18:40:38 -0300

    Create 12 - For3.java

---
+++ curso-java/fundamentos/Modulo02/Extruturas de Controle/12 - For3.java
@@ -0,0 +1,21 @@
+package EstruturasControle;
+
+public class For3 {
+
+	public static void main(String[] args) {
+
+		
+		for(int i = 0; i < 2; i++ ) {
+			
+			for(int j = 0; j < 2; j++) {
+				
+				System.out.printf("[%d %d]", i, j);
+				
+			}
+			
+			System.out.println();
+		}
+
+	}
+
+}
